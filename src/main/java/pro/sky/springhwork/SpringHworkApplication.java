0d757c18a0a235925284d3f5370e02eb955c2bd2
Tatiana <tanja6122212@list.ru>
2022-01-29 23:40:14 +0900

    Initial commit

---
+++ src/main/java/pro/sky/springhwork/SpringHworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springhwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHworkApplication.class, args);
+	}
+
+}
